e9c2fabd6ecacd135b8d1665c8effa81025b2949
Maria Rosa Rosete <ross.zhena@gmail.com>
2018-07-30 17:22:06 -0500

    Se estructura codigo

--- src/data.js
+++ src/data.js
@@ -1,65 +1,102 @@
-window.computeStudentStats = (laboratoria) => {
+window.computeStudentsStats = (laboratoria) => {
   let arrayStudents = [];
   for (key in laboratoria) {
     for (item in laboratoria[key].generacion) {
-    const student = {
-    name: null,
-    email: null,
-    campus: null,
-    generation: null,
-    stats: {status: null,
-            completedPercentage: null,
-            topics:{completedPercentage: null,
-                    percentageDuration: null,
-                    subtopics:{completedPercentage: null,
-                               type: null,
-                               duration: null
-                              }
-                  },
-          }
-  };
-  student.campus = key; //sedes
-  student.generation = item;
- // student.name = laboratoria[key].generacion[item].estudiantes.
-}}
-};
+      for(stud in laboratoria[key].generacion[item]){
+      
 
-window.computeGenerationStats = (laboratoria) => {
-  // Devolver un nuevo arreglo de generaciones
+      for (i in laboratoria[key].generacion[item].estudiantes) {  
+        const student = {
+                      name: null,
+                      email: null,
+                      campus: null,
+                      generation: null,
+                      stats: {status: null,
+                      completedPercentage: null,
+                      topics:{completedPercentage: null,
+                              percentageDuration: null,
+                              subtopics:{completedPercentage: null,
+                                        type: null,
+                                        duration: null
+                                        }
+                              },
+                              }
+                        };
+      student.campus = key; //sedes
+      student.generation = item;
+        student.name = laboratoria[key].generacion[item].estudiantes[i].nombre;
+        student.email = laboratoria[key].generacion[item].estudiantes[i].correo;
+        student.campus = key;
+        student.generation = item;
+
+        let status;
+          if(laboratoria[key].generacion[item].estudiantes[i].progreso.porcentajeCompletado>90){
+            status = "Buen Estudiante"
+            } else if(laboratoria[key].generacion[item].estudiantes[i].progreso.porcentajeCompletado<60){
+              status = "Estudiante con dificultades"
+            } else{
+              status = "Promedio estudiante"
+            }
+        student.stats.status = status;
+        student.stats.completedPercentage = laboratoria[key].generacion[item].estudiantes[i].progreso.porcentajeCompletado;
+
+         let objectTheme = laboratoria[key].generacion[item].estudiantes[i].progreso.temas;
+      
+         for(studentLab in student){
+        for(theme in objectTheme) {
+        student.stats.topics.completedPercentage = laboratoria[key].generacion[item].estudiantes[i].progreso.temas[theme].porcentajeCompletado;
+        student.stats.topics.percentageDuration = laboratoria[key].generacion[item].estudiantes[i].progreso.temas[theme].duracionTemaCompletado;
+
+        let objectSubTheme = laboratoria[key].generacion[item].estudiantes[i].progreso.temas[theme].subtemas;
+
+            for(subtheme in objectSubTheme) {
+              student.stats.topics.subtopics.completedPercentage = objectSubTheme[subtheme].completado;
+              student.stats.topics.subtopics.type = objectSubTheme[subtheme].tipo;
+              student.stats.topics.subtopics.duration = objectSubTheme[subtheme].duracionSubtema; 
+            }     
+        }
+      }
+   
+        arrayStudents.push(student);
+     
+        }
+       
+      }
+      
+    }
+    
+  }
+  return arrayStudents
+}
+
+window.computeGenerationsStats = (laboratoria) => {
   let arrayGenerations = [];  
   for (key in laboratoria) {
-    //console.log(generations.campus = key) //trae las sedes
-  // console.log(laboratoria[key].generacion) //trae el objeto de generaciones
     for (item in laboratoria[key].generacion) {
-      const generations = {
+        const generations = {
         generation: null,
         campus: null,
-        count: null,
-        average: null
+        count: 0,
+        average: 0
       };
       generations.campus = key; //sedes
-      generations.generation = item;
-      //console.log(item) //trae el texto de las generaciones
-      generations.count = laboratoria[key].generacion[item].estudiantes.length;
-      //console.log(generations.count)//imprime el total de estudiantes por generacion 
-      let students = laboratoria[key].generacion[item].estudiantes; // ESTO ES UN ARREGLO DE ESTUDIANTES
-    // console.log(students)// imprime objeto con el total de estudiantes y sus estudiantes
+      generations.generation = item; //generaciones
+      generations.count = laboratoria[key].generacion[item].estudiantes.length;//imprime el total de estudiantes por generacion
+      let students = laboratoria[key].generacion[item].estudiantes; // imprime objeto con el total de estudiantes y sus estudiantes
       let sumaTotal = students.reduce((prev, current)=>{
-        return prev + current.progreso.porcentajeCompletado;
-      }, 0) ;
-      generations.average = Math.round(sumaTotal / generations.count);
-      //console.log(generations.average);
-     // console.log(generations)
-      //console.log(arrayGenerations);
-      
-      arrayGenerations.unshift(generations);
-    //   students.forEach((student) => {
-    //     console.log(student.progreso.porcentajeCompletado)
-    //   })
- 
+          return prev + current.progreso.porcentajeCompletado;
+        }, 0) ;
+        generations.average = Math.round(sumaTotal / students.length);
+        arrayGenerations.push(generations); 
     }//llave del for
-    //console.log(generations)
   }
-  console.log(arrayGenerations);
   return arrayGenerations;
-};
+}; 
+
+window.sortStudents = (students, orderBy, orderDirection) => {
+
+  }
+
+window.filterStudents = (students, search) => {
+
+}
--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ window.onload = () =>{
     fetch('../data/laboratoria.json')
     .then(res =>res.json())
     .then(data =>{
-      let generations = computeGenerationStats(data);
-    
-      //let students = computeStudentStats(data);
+      let generations = computeGenerationsStats(data);
+       let students = computeStudentsStats(data);
+     // console.log(students)
       sedes(data);
-      listeners(generations)
+      listenersSedes(generations, students)  
     })
     }
     //funcion para imprimir sedes
@@ -18,37 +18,78 @@ window.onload = () =>{
       }}
     //fin función sedes
     
-    const listeners = (generations) => {
+    const listenersSedes = (generations, students) => {
       const linkSedes = document.getElementsByClassName('sedes'); // Colección HTML
           for (let i = 0; i < linkSedes.length; i++) {
           linkSedes[i].addEventListener('click', (event) => {
-          let campus = event.target.innerHTML;
-    
-          drawGenerations(generations, campus);
+          let campus = event.target.innerHTML; //sede
+          drawGenerations(generations, campus, students);
         });
-    
+    /*
         const linkGen = document.getElementsByClassName('generacion'); // Colección HTML
         for (let i = 0; i < linkGen.length; i++) {
           linkGen[i].addEventListener('click', (event) => {
-        let campus = event.target.innerHTML;
-    
-      });
+          let std = event.target.innerHTML;
+          console.log(std)
+          drawStudents(generations, std, students);
+   
+          }); 
+        }*/
+        
     }
     
+  
+  }
+    const listenersGen = (generations, students) => {
+    
+       
     }
     
     //funcion para imprimir generaciones
-    const drawGenerations = (generations, campus) => {
-      //console.log(generations)
+    const drawGenerations = (generations, campus, students) => {
+     
       const filteredGenerations = generations.filter((generation) => {
           return generation.campus === campus;
       });
       const containerGen = document.getElementById(`gen-${campus}`);
-      console.log(containerGen)
+     //console.log(containerGen)
       let templateGeneration = ``;
       filteredGenerations.forEach((gen) => {
-       
+      //console.log(gen)
           templateGeneration += `<a class="nav-link generacion"  href="#" id="'+item+'">${gen.generation}</a>`
-          containerGen.innerHTML= templateGeneration                 
+          containerGen.innerHTML= templateGeneration   
+          listenersGen(generations, students)            
       })
-    }}//fin funcion drawGenerations
+      const linkGen = document.getElementsByClassName('generacion'); // Colección HTML
+      //console.log(linkGen)
+      for (let i = 0; i < linkGen.length; i++) {
+      linkGen[i].addEventListener('click', (event) => {
+      let gen = event.target.innerHTML;
+      drawStudents(gen, students, campus);
+
+      }); 
+    }
+    }//fin funcion drawGenerations
+
+    //funcion para imprimir estudiantes
+    const drawStudents = (gen, students, campus) => {
+    //  console.log(campus)
+      const filteredStudentsByCampus = students.filter((student) => {
+        return student.campus === campus
+      });
+
+      const filteredStudentsByGeneration = filteredStudentsByCampus.filter((student) => {
+        return student.generation === gen;
+      })
+     // console.log(filteredStudentsByGeneration)
+    const containerGen2 = document.getElementById(`gen-${campus}`);
+     let templateGeneration2 = ``;
+     filteredStudentsByCampus.forEach((gen) => {
+     console.log(gen)
+         templateGeneration2 += `<a class="nav-link generacion"  href="#" id="'+item+'">${gen.name}</a>`
+         containerGen2.innerHTML= templateGeneration2   
+        // listenersGen(generations, students)            
+     })
+
+     }//fin funcion drawGenerations
+    
